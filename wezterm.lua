0925cd57a61f4bfeaf7c2608a2b392c1added968
hiimnhan <35358825+hiimnhan@users.noreply.github.com>
2024-11-18 14:55:38 +0700

    Update wezterm.lua

--- wezterm.lua
+++ wezterm.lua
@@ -7,7 +7,7 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("JetBrains Mono", { weight = "Bold" })
 config.font_size = 15
 config.color_scheme = "Tokyo Night"
-config.window_background_opacity = 0.8
+config.window_background_opacity = 0.9
 
 -- credit goes to https://www.youtube.com/watch?v=V1X4WQTaxrc
 -- tab bar
@@ -15,109 +15,141 @@ config.hide_tab_bar_if_only_one_tab = false
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = true
-wezterm.on("update-right-status", function(window, _)
-  local SOLID_LEFT_ARROW = ""
-  local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
-  local prefix = ""
+wezterm.on("update-right-status", function(window, pane)
+	local SOLID_LEFT_ARROW = ""
+	local ARROW_FOREGROUND = { Foreground = { Color = "#c6a0f6" } }
+	local prefix = ""
 
-  if window:leader_is_active() then
-    prefix = " " .. utf8.char(0x1f30a) -- ocean wave
-    SOLID_LEFT_ARROW = utf8.char(0xe0b2)
-  end
+	window:set_right_status(window:active_workspace())
 
-  if window:active_tab():tab_id() ~= 0 then
-    ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
-  end -- arrow color based on if tab is first pane
+	if window:leader_is_active() then
+		prefix = " " .. utf8.char(0x1f30a) -- ocean wave
+		SOLID_LEFT_ARROW = utf8.char(0xe0b2)
+	end
 
-  window:set_left_status(wezterm.format({
-    { Background = { Color = "#b7bdf8" } },
-    { Text = prefix },
-    ARROW_FOREGROUND,
-    { Text = SOLID_LEFT_ARROW },
-  }))
+	if window:active_tab():tab_id() ~= 0 then
+		ARROW_FOREGROUND = { Foreground = { Color = "#1e2030" } }
+	end -- arrow color based on if tab is first pane
+
+	window:set_left_status(wezterm.format({
+		{ Background = { Color = "#b7bdf8" } },
+		{ Text = prefix },
+		ARROW_FOREGROUND,
+		{ Text = SOLID_LEFT_ARROW },
+	}))
 end)
 -- tmux-like keybindings
-config.leader = { key = "a", mods = "ALT", timeout_milliseconds = 2000 }
+config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 }
 config.keys = {
-  {
-    mods = "LEADER",
-    key = "c",
-    action = wezterm.action.SpawnTab("CurrentPaneDomain"),
-  },
-  {
-    mods = "LEADER",
-    key = "x",
-    action = wezterm.action.CloseCurrentPane({ confirm = true }),
-  },
-  {
-    mods = "LEADER",
-    key = "b",
-    action = wezterm.action.ActivateTabRelative(-1),
-  },
-  {
-    mods = "LEADER",
-    key = "n",
-    action = wezterm.action.ActivateTabRelative(1),
-  },
-  {
-    mods = "LEADER",
-    key = "|",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    mods = "LEADER",
-    key = "-",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
-  },
-  {
-    mods = "LEADER",
-    key = "h",
-    action = wezterm.action.ActivatePaneDirection("Left"),
-  },
-  {
-    mods = "LEADER",
-    key = "j",
-    action = wezterm.action.ActivatePaneDirection("Down"),
-  },
-  {
-    mods = "LEADER",
-    key = "k",
-    action = wezterm.action.ActivatePaneDirection("Up"),
-  },
-  {
-    mods = "LEADER",
-    key = "l",
-    action = wezterm.action.ActivatePaneDirection("Right"),
-  },
-  {
-    mods = "LEADER",
-    key = "LeftArrow",
-    action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "RightArrow",
-    action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "DownArrow",
-    action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
-  },
-  {
-    mods = "LEADER",
-    key = "UpArrow",
-    action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
-  },
+	{
+		mods = "LEADER",
+		key = "c",
+		action = wezterm.action.SpawnTab("CurrentPaneDomain"),
+	},
+	{
+		mods = "LEADER",
+		key = "x",
+		action = wezterm.action.CloseCurrentPane({ confirm = true }),
+	},
+	{
+		mods = "LEADER",
+		key = "b",
+		action = wezterm.action.ActivateTabRelative(-1),
+	},
+	{
+		mods = "LEADER",
+		key = "p",
+		action = wezterm.action.ActivateTabRelative(1),
+	},
+	{
+		mods = "LEADER",
+		key = "|",
+		action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "-",
+		action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }),
+	},
+	{
+		mods = "LEADER",
+		key = "h",
+		action = wezterm.action.ActivatePaneDirection("Left"),
+	},
+	{
+		mods = "LEADER",
+		key = "j",
+		action = wezterm.action.ActivatePaneDirection("Down"),
+	},
+	{
+		mods = "LEADER",
+		key = "k",
+		action = wezterm.action.ActivatePaneDirection("Up"),
+	},
+	{
+		mods = "LEADER",
+		key = "l",
+		action = wezterm.action.ActivatePaneDirection("Right"),
+	},
+	{
+		mods = "LEADER",
+		key = "LeftArrow",
+		action = wezterm.action.AdjustPaneSize({ "Left", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "RightArrow",
+		action = wezterm.action.AdjustPaneSize({ "Right", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "DownArrow",
+		action = wezterm.action.AdjustPaneSize({ "Down", 5 }),
+	},
+	{
+		mods = "LEADER",
+		key = "UpArrow",
+		action = wezterm.action.AdjustPaneSize({ "Up", 5 }),
+	},
+	-- workspace
+	{
+		mods = "LEADER",
+		key = "s",
+		action = wezterm.action.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }),
+	},
+	{
+		mods = "LEADER",
+		key = "w",
+		action = wezterm.action.PromptInputLine({
+			description = wezterm.format({
+				{ Attribute = { Intensity = "Bold" } },
+				{ Foreground = { AnsiColor = "Fuchsia" } },
+				{ Text = "Enter name for new workspace" },
+			}),
+			action = wezterm.action_callback(function(window, pane, line)
+				-- line will be `nil` if they hit escape without entering anything
+				-- An empty string if they just hit enter
+				-- Or the actual line of text they wrote
+				if line then
+					window:perform_action(
+						wezterm.action.SwitchToWorkspace({
+							name = line,
+						}),
+						pane
+					)
+				end
+			end),
+		}),
+	},
 }
 
 for i = 0, 9 do
-  -- leader + number to activate that tab
-  table.insert(config.keys, {
-    key = tostring(i),
-    mods = "LEADER",
-    action = wezterm.action.ActivateTab(i),
-  })
+	-- leader + number to activate that tab
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = wezterm.action.ActivateTab(i),
+	})
 end
 
 -- and finally, return the configuration to wezterm
