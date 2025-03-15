bb96955c99794f919cd0412454ca65aa63e946a6
nhanncv <vinhnhan1999bc@gmail.com>
2025-03-15 23:16:24 +0700

    spaceship config + update with symlink

---
+++ spaceship.zsh
@@ -0,0 +1,13 @@
+# Display time
+SPACESHIP_TIME_SHOW=true
+
+SPACESHIP_AZURE_SHOW=false
+SPACESHIP_AWS_SHOW=false
+
+# Display username always
+SPACESHIP_USER_SHOW=always
+SPACESHIP_USER_PREFIX=''
+
+# Do not truncate path in repos
+SPACESHIP_DIR_TRUNC_REPO=false
+
